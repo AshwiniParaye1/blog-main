c93cc563b1bb9d817939db64ec6b33882f605d02
Ashwini <ashwiniparaye9309@gmail.com>
2023-05-22 09:08:30 +0530

    changes

--- pages/api/posts/[postId].js
+++ pages/api/posts/[postId].js
@@ -1,14 +1,19 @@
-import data from "../data"
+import data from "../data";
 
 // api/posts/1
 export default function handler(req, res) {
-  const { postId } = req.query
-  const { Posts } = data
+  const { postId } = req.query;
+  const { Posts } = data;
 
   if (postId) {
-    const post = Posts.find((value) => value.id == postId)
-    return res.status(200).json(post)
+    const post = Posts.find((value) => value.id == postId);
+
+    if (post) {
+      return res.status(200).json(post);
+    } else {
+      return res.status(404).json({ error: "Post Not Found" });
+    }
   }
 
-  return res.status(404).json({ error: "Post Not Found" })
+  return res.status(404).json({ error: "Post Not Found" });
 }
